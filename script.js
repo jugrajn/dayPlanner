06bf37bee7d5122eb66dc8c093738bf5a4218c35
jugrajn <jugraj.nijjar@gmail.com>
2021-05-27 21:48:56 -0400

    added function to display the return value back into textarea element

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var saveEl = document.querySelector(".far")
 
 //variable to access the textarea
-var textEl = document.querySelector(".description").
+var textEl = $(".description");
 
 
 //If I click Save icon, whate I've typed should save
@@ -10,11 +10,18 @@ saveEl.addEventListener('click', function(event) {
     event.preventDefault();
 
     //set variable to get access to textarea value
-    var textValue = document.querySelector("description").value
+    var textValue = $(".description").value
 
     //Store the textarea value
     localStorage.setItem("text", textValue);
-    //Get Text
-    localStorage.getItem("text")
+    //Get Text New function
+    displayMessage()
+    
 });
+//Now display the message that was store by getting it back
+function displayMessage () {
 
+    // locally declare value so you can use it put it back into textarea element
+    var textValue = localStorage.getItem("text");
+    textEl.text(textValue);
+}
