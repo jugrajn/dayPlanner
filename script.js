562279c6b28f67e67f392e10c00bd8881db5f7f0
jugrajn <jugraj.nijjar@gmail.com>
2021-05-28 19:13:47 -0400

    starting with ready function and putting all my code inside to make sure javascript initiates first

--- script.js
+++ script.js
@@ -4,32 +4,39 @@ var saveEl = $('.far')
 //variable to access the textarea
 var textEl = $('.description');
 
+$(document).ready(function() {
 
 //If I click Save icon, whate I've typed should save
-saveEl.on('click', function(event) {
-    event.preventDefault();
+    $('.far').on('click', function() {
+        // event.preventDefault();
+    
+        //set variable to get access to textarea value
+        var textValue = $(this).siblings('.description').val();
+    
+        //Store the textarea value
+        localStorage.setItem('textValue', textValue);
+        //Get Text New function
+        displayMessage()
+        console.log(textValue);
+    });
 
-    //set variable to get access to textarea value
-    var textValue = $(this).closest('.description').val();
+    function displayMessage() {
 
-    //Store the textarea value
-    localStorage.setItem('textValue', textValue);
-    //Get Text New function
-    displayMessage()
-    console.log(textValue);
-});
+        // locally declare value so you can use it put it back into FOR EACH textarea element
+         $('#am9 .description').val(localStorage.getItem('textValue'));
+         $('#am10 .description').val(localStorage.getItem('textValue'));
+         $('#am11 .description').val(localStorage.getItem('textValue'));
+         $('#pm12 .description').val(localStorage.getItem('textValue'));
+         $('#pm1 .description').val(localStorage.getItem('textValue'));
+         $('#pm2 .description').val(localStorage.getItem('textValue'));
+         $('#pm3 .description').val(localStorage.getItem('textValue'));
+         $('#pm4 .description').val(localStorage.getItem('textValue'));
+         $('#pm5 .description').val(localStorage.getItem('textValue'));
+    
+    }
+    
+})
+
+
+
 //Now display the message that was store by getting it back
-function displayMessage() {
-
-    // locally declare value so you can use it put it back into FOR EACH textarea element
-     $('#am9 .description').val(localStorage.getItem('textValue'));
-     $('#am10 .description').val(localStorage.getItem('textValue'));
-     $('#am11 .description').val(localStorage.getItem('textValue'));
-     $('#pm12 .description').val(localStorage.getItem('textValue'));
-     $('#pm1 .description').val(localStorage.getItem('textValue'));
-     $('#pm2 .description').val(localStorage.getItem('textValue'));
-     $('#pm3 .description').val(localStorage.getItem('textValue'));
-     $('#pm4 .description').val(localStorage.getItem('textValue'));
-     $('#pm5 .description').val(localStorage.getItem('textValue'));
-
-}
