2ac13c170f5542f74c74b6bf96c987f08f5d3eb0
jugrajn <jugraj.nijjar@gmail.com>
2021-05-27 21:17:48 -0400

    added event listener click on save button

---
+++ script.js
@@ -0,0 +1,14 @@
+//variable for save icon
+var saveEl = document.querySelector(".far")
+
+//variable for text area 
+var textEl = document.querySelector(".description")
+
+// LocalStorage Save text Function
+function saveText() {
+    
+}
+
+//If I click Save icon, whate I've typed should save
+saveEl.addEventListener('click', saveText);
+
