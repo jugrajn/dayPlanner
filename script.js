fa11f0b035032421f9437942cf1d3a827fee1a47
jugrajn <jugraj.nijjar@gmail.com>
2021-05-29 14:07:08 -0400

    had to change ids from 1pm onwards to military time for conditions to work

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-//variable for save icon
-var saveEl = $('.far')
 
-//variable to access the textarea
-var textEl = $('.description');
-
-    //If I click Save icon, whate I've typed should save
+   //If I click Save icon, whate I've typed should save
     $('.saveBtn').on('click', function() {
         console.log("im here")
     
@@ -19,15 +14,16 @@ var textEl = $('.description');
 
     function displayMessage() {
         // locally declare value so you can use it put it back into FOR EACH textarea element
-         $('#am9 .description').val(localStorage.getItem('am9'));
-         $('#am10 .description').val(localStorage.getItem('am10'));
-         $('#am11 .description').val(localStorage.getItem('am11'));
-         $('#pm12 .description').val(localStorage.getItem('pm12'));
-         $('#pm1 .description').val(localStorage.getItem('pm1'));
-         $('#pm2 .description').val(localStorage.getItem('pm2'));
-         $('#pm3 .description').val(localStorage.getItem('pm3'));
-         $('#pm4 .description').val(localStorage.getItem('pm4'));
-         $('#pm5 .description').val(localStorage.getItem('pm5'));    
+         $('#hr9 .description').val(localStorage.getItem('hr9'));
+         $('#hr10 .description').val(localStorage.getItem('hr10'));
+         $('#hr11 .description').val(localStorage.getItem('hr11'));
+         $('#hr12 .description').val(localStorage.getItem('hr12'));
+         $('#hr13 .description').val(localStorage.getItem('hr13'));
+         $('#hr14 .description').val(localStorage.getItem('hr14'));
+         $('#hr15 .description').val(localStorage.getItem('hr15'));
+         $('#hr16 .description').val(localStorage.getItem('hr16'));
+         $('#hr17 .description').val(localStorage.getItem('hr17'));    
+         //Had to use military time for conditions to work based on consol.log results in line 41. 
     }
     displayMessage();
 
@@ -35,13 +31,28 @@ var textEl = $('.description');
     var today = moment();
     $('#currentDay').text(today.format('MMM Do YYYY, h:mm a'));
 
-
     function trackTime() {
         //define the current time in hours with the help of moment.js
         var hourNow = moment().hour();
 
         //loop through time blocks using jquery syntax
-        
+        $('.time-block').each(function () {
+            var hourBlock = parseInt($(this).attr('id').split('hr')[1]);
+           console.log(hourBlock);
+            console.log(hourNow);
+            if (hourBlock < hourNow) {
+                $(this).addClass('past');
+            }
+            else if (hourBlock === hourNow) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            }
+            else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+
+        });
     }
-
-
+    trackTime();
